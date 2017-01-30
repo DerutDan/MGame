eadaf3a14d0528dde0eef95d9ccd68a01f18e446
Дерут Даниил <danilderut@gmail.com>
2017-01-30 23:04:48 +0300

    Client changed a lot

--- MServer/src/GameBoard.java
+++ MServer/src/GameBoard.java
@@ -18,16 +18,20 @@ public class GameBoard {
     ArrayList<Monster> cMonstersP2 = new ArrayList<>();
     ArrayList<Monster> monstersP1 = new ArrayList<>();
     ArrayList<Monster> monstersP2 = new ArrayList<>();
-    Socket player1Socket,player2Socket;
     ObjectOutputStream oout1,oout2;
     DataInputStream din1,din2;
+    InputStream sin1,sin2;
+    OutputStream sout1,sout2;
+
     volatile boolean upToDate = false;
     Thread updater = upDate();
     Random rand = new Random();
-    GameBoard(Socket _player1Socket,Socket _player2ocket)
+    GameBoard(InputStream _sin1,OutputStream _sout1,InputStream _sin2, OutputStream _sout2)
     {
-        player1Socket = _player1Socket;
-        player2Socket = _player2ocket;
+        sin1 = _sin1;
+        sin2 = _sin2;
+        sout1 = _sout1;
+        sout2 = _sout2;
         initAll();
         deckP1 = getDeck();
         deckP2 = getDeck();
@@ -42,21 +46,15 @@ public class GameBoard {
         isGameOn = true;
 
         try {
-            InputStream sin1 = player1Socket.getInputStream();
-            OutputStream sout1 = player1Socket.getOutputStream();
             sout1.write(1);
+            sout2.write(2);
             din1 = new DataInputStream(sin1);
             oout1 = new ObjectOutputStream(sout1);
-            InputStream sin2 = player2Socket.getInputStream();
-            OutputStream sout2 = player2Socket.getOutputStream();
-            sout2.write(2);
+
             din2 = new DataInputStream(sin2);
             oout2 = new ObjectOutputStream(sout2);
         } catch (IOException e) {
-            if(player1Socket.isOutputShutdown()) System.out.println("Disconnected1");
-            else e.printStackTrace();
-            if(player2Socket.isOutputShutdown()) System.out.println("Disconnected2");
-            else e.printStackTrace();
+            e.printStackTrace();
         }
 
 
@@ -155,6 +153,7 @@ public class GameBoard {
 
     void Phase01()
     {
+        System.out.println(1);
        wForUpDate();
         if(handP1.size() <=StaticVariables.handSize) {
             handP1.add(takeCard(deckP1));
@@ -168,40 +167,59 @@ public class GameBoard {
             }
         }
         upToDate = false;
+        System.out.println(2);
         Phase11();
     }
 
     void Phase11()
     {
+        System.out.println(3);
         wForUpDate();
         int cardIndex = reader1();
-        GameCard out = handP1.get(cardIndex);
-        String type = out.type;
-        switch(type)
-        {
-            case "Monster" : { cMonstersP1.add((Monster)out); break;}
-            case "Weapon" : { P1.equipWeapon((Weapon)out); break;}
-            case "Armor" : { P1.equipArmor((Armor)out); break;}
+        System.out.println(cardIndex);
+        if(cardIndex >=  0 && cardIndex < handP1.size()) {
+            GameCard out = handP1.get(cardIndex);
+            String type = out.type;
+            switch (type) {
+                case "Monster": {
+                    cMonstersP1.add((Monster) out);
+                    break;
+                }
+                case "Weapon": {
+                    P1.equipWeapon((Weapon) out);
+                    break;
+                }
+                case "Armor": {
+                    P1.equipArmor((Armor) out);
+                    break;
+                }
+            }
+            handP1.remove(cardIndex);
+            upToDate = false;
         }
-        upToDate = false;
+        System.out.println(4);
         Phase21();
     }
 
     void Phase21()
     {
+        System.out.println(5);
         int cardIndex = reader1();
-        if(cardIndex == -1 || cardIndex >= monstersP2.size())
-            return;
-        Monster m = monstersP2.get(cardIndex);
-        P1.getHit(m.getAt());
-        m.getHit(P1.getAt());
-        if(!m.isAlive()) monstersP2.remove(cardIndex);
-        upToDate = false;
+        System.out.println(cardIndex);
+        if(cardIndex >=  0 && cardIndex < monstersP2.size()) {
+            Monster m = monstersP2.get(cardIndex);
+            P1.getHit(m.getAt());
+            m.getHit(P1.getAt());
+            if (!m.isAlive()) monstersP2.remove(cardIndex);
+            upToDate = false;
+        }
+        System.out.println(6);
         Phase31();
     }
 
     void Phase31()
     {
+        System.out.println(7);
         wForUpDate();
         int sum = 0;
         for(int  i = 0 ; i < monstersP2.size();++i)
@@ -211,12 +229,14 @@ public class GameBoard {
         P1.getHit(sum);
         if(!P1.isAlive()) isGameOn = false;
         upToDate = false;
+        System.out.println(8);
         Phase02();
     }
 
     //P2 phases
     void Phase02()
     {
+        System.out.println(9);
         wForUpDate();
         if(handP2.size() <=StaticVariables.handSize){
             handP2.add(takeCard(deckP2));
@@ -230,41 +250,61 @@ public class GameBoard {
             }
         }
         upToDate = false;
+        System.out.println(10);
         Phase12();
     }
 
     void Phase12()
     {
+        System.out.println(11);
         wForUpDate();
         int cardIndex = reader2();
-        GameCard out = handP2.get(cardIndex);
-        String type = out.type;
-        switch(type)
-        {
-            case "Monster" : { cMonstersP2.add((Monster)out); break;}
-            case "Weapon" : { P2.equipWeapon((Weapon)out); break;}
-            case "Armor" : { P2.equipArmor((Armor)out); break;}
+        System.out.println(cardIndex);
+        if(cardIndex >=  0 && cardIndex < handP2.size()) {
+            GameCard out = handP2.get(cardIndex);
+            String type = out.type;
+            switch (type) {
+                case "Monster": {
+                    cMonstersP2.add((Monster) out);
+                    break;
+                }
+                case "Weapon": {
+                    P2.equipWeapon((Weapon) out);
+                    break;
+                }
+                case "Armor": {
+                    P2.equipArmor((Armor) out);
+                    break;
+                }
+            }
+            handP2.remove(cardIndex);
+            upToDate = false;
         }
-        upToDate = false;
+        System.out.println(12);
         Phase22();
+
     }
 
     void Phase22()
     {
+        System.out.println(13);
         wForUpDate();
         int cardIndex = reader2();
-        if(cardIndex == -1 || cardIndex >= monstersP1.size())
-            return;
-        Monster m = monstersP1.get(cardIndex);
-        P2.getHit(m.getAt());
-        m.getHit(P2.getAt());
-        if(!m.isAlive()) monstersP1.remove(cardIndex);
-        upToDate = false;
+        System.out.println(cardIndex);
+        if(cardIndex >=  0 && cardIndex < monstersP1.size()) {
+            Monster m = monstersP1.get(cardIndex);
+            P2.getHit(m.getAt());
+            m.getHit(P2.getAt());
+            if (!m.isAlive()) monstersP1.remove(cardIndex);
+            upToDate = false;
+        }
+        System.out.println(14);
         Phase32();
     }
 
     void Phase32()
     {
+        System.out.println(15);
         wForUpDate();
         int sum = 0;
         for(int  i = 0 ; i < monstersP1.size();++i)
@@ -274,7 +314,9 @@ public class GameBoard {
         P2.getHit(sum);
         if(!P2.isAlive()) isGameOn = false;
         upToDate = false;
+        System.out.println(16);
         Phase01();
+
     }
 
     //P1: hp def at weap arm cmosterNum AllMonsterIndexes monsterNum AllMonsterIndexes(curhp) P2 hp def at
@@ -309,9 +351,9 @@ public class GameBoard {
             {
                 oout1.writeObject(monstersP2.get(i));
             }
+            oout1.flush();
         } catch (IOException e) {
-            if(player1Socket.isOutputShutdown() || player1Socket.isClosed()) System.out.println("Disconnected1");
-            else e.printStackTrace();
+           e.printStackTrace();
         }
 
     }
@@ -347,10 +389,11 @@ public class GameBoard {
             {
                 oout2.writeObject(monstersP1.get(i));
             }
+            oout2.flush();
         } catch (IOException e) {
-            if(player2Socket.isOutputShutdown() || player2Socket.isClosed()) System.out.println("Disconnected2");
-            else e.printStackTrace();
+            e.printStackTrace();
         }
+
 
     }
 
@@ -376,8 +419,7 @@ public class GameBoard {
         try {
             return din1.readInt();
         } catch (IOException e) {
-            if(player1Socket.isInputShutdown()) System.out.println("Disconnected1");
-            else e.printStackTrace();
+            e.printStackTrace();
         }
         return 0;
     }
@@ -387,8 +429,7 @@ public class GameBoard {
         try {
             return din2.readInt();
         } catch (IOException e) {
-            if(player2Socket.isInputShutdown()) System.out.println("Disconnected2");
-            else e.printStackTrace();
+            e.printStackTrace();
         }
         return 0;
     }
--- MServer/src/Main.java
+++ MServer/src/Main.java
@@ -2,36 +2,63 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Main {
+import static java.lang.Thread.sleep;
 
+public class Main {
+    volatile static Socket player1Socket = null,player2Socket = null;
+    static InputStream sin1 = null,sin2 = null;
+    static OutputStream sout1 = null,sout2 = null;
+    volatile static boolean flag = true;
     public static void main(String[] args)
     {
+
         int port = 5555;
         try {
+
+
+
             ServerSocket ss = new ServerSocket(port);
-            Socket player1Socket = ss.accept();
-            System.out.println("Conneted1");
-            Socket player2Socket = ss.accept();
-            System.out.println("Conneted2");
+            //Thread ch1 = checker1(), ch2 = checker2();
+            //ch1.start();
+            //ch2.start();
+            while(player1Socket == null || player2Socket == null || player1Socket.isClosed() || player2Socket.isClosed()) {
 
-            while(player1Socket.isClosed() && player2Socket.isClosed()) {
-
-
-                if(!player1Socket.isClosed()) {
+                if(player1Socket == null || player1Socket.isClosed()) {
                     player1Socket = ss.accept();
                     System.out.println("Conneted1");
+                    check2();
                 }
-                if(!player2Socket.isClosed()) {
+                try{
+                    sin1 = player1Socket.getInputStream();
+                    sout1 = player1Socket.getOutputStream();
+
+
+                } catch (IOException e)
+                {
+                    e.printStackTrace();
+
+                }
+                if(player2Socket == null || player2Socket.isClosed()) {
                     player2Socket = ss.accept();
                     System.out.println("Conneted2");
+                    check1();
                 }
+
+                try{
+                    sin2 = player2Socket.getInputStream();
+                    sout2 = player2Socket.getOutputStream();
+
+                } catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+
             }
+            //ch1.interrupt();
+            //ch2.interrupt();
+            System.out.println("Starts");
+            GameBoard game = new GameBoard(sin1, sout1, sin2, sout2);
 
-
-
-
-
-        GameBoard game = new GameBoard(player1Socket,player2Socket);
 
         game.StartGame();
         } catch (IOException e)
@@ -39,8 +66,62 @@ public class Main {
             System.out.println("Connection failure");
             e.printStackTrace();
         }
-
     }
+    static void check1()
+    {
+        boolean close;
+        try {
+            close = false;
+            if (player1Socket != null && !player1Socket.isClosed()) {
+                if(sout1 != null) sout1.write(3);
+                if(sin1 != null && sin1.read() == 255)
+                {
+                    close = true;
+                }
+            }
+        }catch (IOException e) {
+            close = true;
+        }
+        if(close) try {
+            if(!player1Socket.isClosed())
+            {
+                player1Socket.close();
+                System.out.println("Closed1");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    static void check2()
+    {
+        boolean close;
+        close = false;
+        if (player2Socket != null && !player2Socket.isClosed()) {
+            try {
+                if(sout2 != null)
+                {
+                    sout2.write(3);
+                }
+                if(sin2 != null && sin2.read() == 255)
+                {
+                    close = true;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                close = true;
+            }
+        }
+        if(close) try {
+            if(!player2Socket.isClosed())
+            {
+                player2Socket.close();
+                System.out.println("Closed2");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 
 
 
